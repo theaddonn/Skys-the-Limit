2789748fa1c61a3ab09c8ad60b8049c54f0b6ba3
Adrian <adrian.olbrich1@gmail.com>
2024-05-20 19:00:56 +0200

    added code

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,17 +1,18 @@
 ﻿#include "dllmain.hpp"
 
+SafetyHookInline Dimension_Dimension_Hook;
+
+void* Dimension_Dimension(void* self, void* a2, int dimensionID, HeightRange HeightRange, void* a5, std::string* dimName)
+{
+    // Set max height to 2048 blocks
+    HeightRange.mMaxHeight = 2048;
+    return Dimension_Dimension_Hook.fastcall<void*>(self, a2, dimensionID, HeightRange, a5, dimName);
+}
+
 // Ran when the mod is loaded into the game by AmethystRuntime
 ModFunction void Initialize(AmethystContext* ctx) 
 {
-    // Logging from <Amethyst/Log.h>
-    Log::Info("Hello, Amethyst World!");
-
-    // Add a listener to a built in amethyst event
-    ctx->mEventManager.onStartJoinGame.AddListener(&OnStartJoinGame);
-}
-
-// Subscribed to amethysts on start join game event in Initialize
-void OnStartJoinGame(ClientInstance* client)
-{
-    Log::Info("The player has joined the game!");
+    // Hook dim ctor
+    ctx->mHookManager.RegisterFunction<Dimension_Dimension>("48 89 5C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 55 41 54 41 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 ? 41 8B F9 41 8B D8");
+    ctx->mHookManager.CreateHook<&Dimension_Dimension>(Dimension_Dimension_Hook, Dimension_Dimension);
 }
--- src/dllmain.hpp
+++ src/dllmain.hpp
@@ -7,5 +7,3 @@
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     return TRUE;
 }
-
-void OnStartJoinGame(ClientInstance* client);
