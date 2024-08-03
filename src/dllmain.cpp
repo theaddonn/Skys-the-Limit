0e4ef479267949c5245e94201628a628b52f23f3
theaddon <theaddonn@gmail.com>
2024-08-04 00:40:45 +0200

    afapt to new Amethyst api changes

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,18 +1,22 @@
 ﻿#include "dllmain.hpp"
+#include <minecraft/src/common/world/level/dimension/HeightRange.hpp>
 
-SafetyHookInline Dimension_Dimension_Hook;
+SafetyHookInline _dimensionDimension;
 
-void* Dimension_Dimension(void* self, void* a2, int dimensionID, HeightRange HeightRange, void* a5, std::string* dimName)
+void* dimensionDimension(void* self, void* a2, int dimensionID, HeightRange HeightRange, void* a5, std::string& dimName)
 {
-    // Set max height to 2048 blocks
-    HeightRange.mMaxHeight = 2048;
-    return Dimension_Dimension_Hook.fastcall<void*>(self, a2, dimensionID, HeightRange, a5, dimName);
+	// Set max height to 2048 blocks
+	HeightRange.mMaxHeight = 2048;
+	return _dimensionDimension.fastcall<void*>(self, a2, dimensionID, HeightRange, a5, dimName);
 }
 
 // Ran when the mod is loaded into the game by AmethystRuntime
-ModFunction void Initialize(AmethystContext* ctx) 
+ModFunction void Initialize(AmethystContext& ctx)
 {
-    // Hook dim ctor
-    ctx->mHookManager.RegisterFunction<Dimension_Dimension>("48 89 5C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 55 41 54 41 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 ? 41 8B F9 41 8B D8");
-    ctx->mHookManager.CreateHook<&Dimension_Dimension>(Dimension_Dimension_Hook, Dimension_Dimension);
+	// Initialize Amethyst mod backend
+	Amethyst::InitializeAmethystMod(ctx);
+
+	// Hook dim ctor
+	Amethyst::GetHookManager().RegisterFunction<&dimensionDimension>("48 89 5C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 55 41 54 41 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 ? 41 8B F9 41 8B D8");
+	Amethyst::GetHookManager().CreateHook<&dimensionDimension>(_dimensionDimension, &dimensionDimension);
 }
--- src/dllmain.hpp
+++ src/dllmain.hpp
@@ -1,9 +1,10 @@
 #pragma once
 #include <Windows.h>
-#include <amethyst/runtime/AmethystContext.hpp>
+#include <amethyst/runtime/ModContext.hpp>
+#include <amethyst/runtime/events/GameEvents.hpp>
 
 #define ModFunction extern "C" __declspec(dllexport)
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-    return TRUE;
+	return TRUE;
 }
